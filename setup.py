cb34b1f33820f8cec4d93e27ef7627701ee94e2f
bala-bc <balaganesh.gurunathan@brickendon.com>
2022-07-05 19:07:31 +0530

    pytest tox setup.py added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version='0.0.1',
+    description='its a mlops package',
+    author='bala',
+    package=find_packages(),
+    license='MIT',
+)
+
+
